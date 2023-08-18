3eef4b8a4933f22f3869d0a5c203a256448b3268
Zlatko Babić <zbabac@users.noreply.github.com>
2023-08-18 13:20:42 +0200

    Create vpn.sh with some cyberghost servers
    servers for Serbia, Croatia, Bosnia, UK, France. Default is Serbia.

---
+++ vpn.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# set default server
+SERVER="belgrade-s403-i09.cg-dialup.net"
+# add your favorite servers in the case getopts options below
+while getopts ":hfbcens" opt; do
+  case "$opt" in
+    f)SERVER="paris-s404-i11.cg-dialup.net";;
+    b)SERVER="travnik-s401-i07.cg-dialup.net";;
+    e)SERVER="london-s440-i03.cg-dialup.net";;
+    s)SERVER="belgrade-s403-i09.cg-dialup.net";;
+    n)SERVER="oslo-s409-i11.cg-dialup.net";;
+    c)SERVER="zagreb-s403-i10.cg-dialup.net";;
+    h)echo "usage: $0 [-h] [-b] [-s] [-e] [-n] [-c] [-f]"
+      echo "### -b BiH ###"
+      echo "### -s Srbija ###"
+      echo "### -c Hrvatska ###" 
+      echo "### -e England ###"
+      echo "### -n Norge ###" 
+      echo "### -f France ###"
+      exit;;
+  esac
+done
+openvpn --dev tun --auth-user-pass /home/jokke/.cyberghost/openvpn/auth --client --proto udp --resolv-retry infinite --persist-key --persist-tun --nobind --cipher AES-256-CBC --auth SHA256 --ping 15 --ping-restart 60 --explicit-exit-notify 2 --script-security 2 --remote-cert-tls server --verb 4 --route-delay 5 20 --mute-replay-warnings --remote $SERVER 443 --log /var/log/cyberghost.log --daemon --dhcp-option DOMAIN-ROUTE . --up /usr/local/cyberghost/update-systemd-resolved --up-restart --down /usr/local/cyberghost/update-systemd-resolved --down-pre --ca /usr/local/cyberghost/certs/openvpn/ca.crt --cert /usr/local/cyberghost/certs/openvpn/client.crt --key /usr/local/cyberghost/certs/openvpn/client.key
